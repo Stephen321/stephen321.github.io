ff8431ebc5bda2ce6a74f56f2ad7e568b7d3f0f5
stephen321 <stephen.enniis@gmail.com>
2017-09-09 19:54:37 +0100

    migrate script and fixed url namespace 'home' not being unique

--- EnnisSite/urls.py
+++ EnnisSite/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^home/', include('home.urls')),
     url(r'', include('home.urls')),
 ]
--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from django.contrib import admin
 from . import views
 
 app_name = 'home'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^home$', views.index, name='index'),
 ]
