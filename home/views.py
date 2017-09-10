175a8e1612645d0812aa337e2aae574668922162
stephen321 <stephen.enniis@gmail.com>
2017-09-10 16:18:47 +0100

    added in images and some template html

--- home/views.py
+++ home/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {"test": 15}
-    return render(request, 'home/index.html', context)
+    return render(request, 'home/index.html')
